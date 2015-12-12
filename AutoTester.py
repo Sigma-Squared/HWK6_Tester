caf85e5161b43621748fa681885f7513594a215d
Chamu Rajasekera <chamu.rajasekera@gmail.com>
2015-12-12 01:00:31 -0500

    PEP8 format

--- AutoTester.py
+++ AutoTester.py
@@ -2,82 +2,90 @@ from subprocess import *
 import random
 import sys
 
+
 def main():
-    #Generate the testcases
-    if len(sys.argv)<3:
-        print("Invalid # of arguments. Usage: AutoTester.py #cases exe_path",file=sys.stderr)
-        return  
-    try:
-        N_TESTCASES  = int(sys.argv[1])
-    except ValueError:
-        print("Incorrect  #cases formatting.",file=sys.stderr)
+    if len(sys.argv) < 3:
+        print(
+            "Invalid # of arguments. Usage: AutoTester.py #cases exe_path", file=sys.stderr)
         return
+    try:
+        N_TESTCASES = int(sys.argv[1])
+    except ValueError:
+        print("Incorrect  #cases formatting.", file=sys.stderr)
+        return
+    # Generate the testcases
     print("Generating Test Cases...")
     test_cases = []
-    N_MAXNUMS = 12 #maximum number of integer terms in the input
-    N_MAXINT = 50 #maximum value of an integer
-    BRACKET_PROB = 0.44 #probability of a bracket appearing 
+    N_MAXNUMS = 12  # maximum number of integer terms in the input
+    N_MAXINT = 50  # maximum value of an integer
+    BRACKET_PROB = 0.44  # probability of a bracket appearing
     for i in range(N_TESTCASES):
-        num_nums = random.randint(1,N_MAXNUMS)
-        
-        #create list of rand numbers
-        num_seq = list( map(str,(random.randint(1,N_MAXINT) for _ in range(num_nums))) )
-        
-        #randomly insert operators
-        operators = ('*','/','+','-')
-        for i in range(1,len(num_seq)*2-1,2):
-            num_seq.insert(i,operators[random.randint(0,len(operators)-1)])
-            
-        #randomly insert brackets
-        for i,item in enumerate(num_seq):
+        num_nums = random.randint(1, N_MAXNUMS)
+
+        # create list of rand numbers
+        num_seq = list(map(str, (random.randint(1, N_MAXINT)
+                                 for _ in range(num_nums))))
+
+        # randomly insert operators
+        operators = ('*', '/', '+', '-')
+        for i in range(1, len(num_seq) * 2 - 1, 2):
+            num_seq.insert(i, operators[random.randint(0, len(operators) - 1)])
+
+        # randomly insert brackets
+        for i, item in enumerate(num_seq):
             if item in operators:
-                if (random.random()<BRACKET_PROB):
-                    #insert first bracket
-                    num_seq.insert(i+1,"(")
-                    #insert its complementary pair
+                if (random.random() < BRACKET_PROB):
+                    # insert first bracket
+                    num_seq.insert(i + 1, "(")
+                    # insert its complementary pair
                     while True:
-                        j = random.randint(i+1,len(num_seq)-1)
+                        j = random.randint(i + 1, len(num_seq) - 1)
                         if num_seq[j] != "(" and num_seq[j] not in operators:
-                            num_seq.insert(j+1,")")
+                            num_seq.insert(j + 1, ")")
                             break
         test_cases.append("".join(num_seq))
     print("Done.\nRunning Program...")
     try:
-        output=getOutput(test_cases,sys.argv[2])
+        output = getOutput(test_cases, sys.argv[2])
     except FileNotFoundError:
-        print("'"+sys.argv[2]+"' Not found.",file=sys.stderr)
-        return        
+        print("'" + sys.argv[2] + "' Not found.", file=sys.stderr)
+        return
     print("Done.\nEnumerating Test Cases...")
     wrong = 0
-    for i,test in enumerate(test_cases):
-        print("Test #",i+1)
-        print("Expression:\t",test)
-        print("Program Answer:\t",output[i])
+    for i, test in enumerate(test_cases):
+        print("Test #", i + 1)
+        print("Expression:\t", test)
+        print("Program Answer:\t", output[i])
         correct = float("{:.2f}".format(eval(test)))
-        print("Correct Answer:\t",correct)
-        error = abs(output[i]-correct)
-        if error==0:
+        print("Correct Answer:\t", correct)
+        error = abs(output[i] - correct)
+        if error == 0:
             print("PASS")
-        elif error<0.1:
-            print("PASS, WARNING!",error,"error detected")
-        elif error<1:
-            print("WARNING! Large error of",error,"detected")
+        elif error < 0.1:
+            print("PASS, WARNING!", error, "error detected")
+        elif error < 1:
+            print("WARNING! Large error of", error, "detected")
         else:
-            print("FAIL! Extreme error of",error,"detected")#,file=sys.stderr)
+            print("FAIL! Extreme error of", error,
+                  "detected")  # ,file=sys.stderr)
             wrong += 1
         print("-----------------------------------------")
-    print("Finished. {}/{} Correct ({} wrong). {:.2f}% Failure Rate".format(N_TESTCASES-wrong,N_TESTCASES,wrong,wrong/N_TESTCASES*100))
+    print("Finished. {}/{} Correct ({} wrong). {:.2f}% Failure Rate".format(
+        N_TESTCASES - wrong, N_TESTCASES, wrong, wrong / N_TESTCASES * 100))
 
-def getOutput(testcases,fname):
-    proc = Popen(fname,stdin=PIPE,stdout=PIPE,stderr=PIPE,universal_newlines=True)
-    proc.stdin.write("\n".join(testcases)+"\n#")
+
+def getOutput(testcases, fname):
+    proc = Popen(fname, stdin=PIPE, stdout=PIPE,
+                 stderr=PIPE, universal_newlines=True)
+    proc.stdin.write("\n".join(testcases) + "\n#")
     proc.stdin.close()
-    #take only the non-empty lines and remove the trailing newline
-    output = [line.rstrip() for line in proc.stdout.readlines() if len(line)>1]
+    # take only the non-empty lines and remove the trailing newline
+    output = [line.rstrip()
+              for line in proc.stdout.readlines() if len(line) > 1]
     proc.kill()
-    output.pop() #remove last useless string
-    for i,string in enumerate(output):
-        index = string.find('=')+2
+    output.pop()  # remove last useless string
+    for i, string in enumerate(output):
+        index = string.find('=') + 2
         output[i] = string[index:]
     return tuple(map(float, output))
 
